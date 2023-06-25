d02c6202b37ff48ede95834d479bfbc59bc56d40
ayshubala <127235354+ayshubala@users.noreply.github.com>
2023-06-25 14:07:16 +0530

    Update armygame.php

--- armygame.php
+++ armygame.php
@@ -1,24 +1,31 @@
 <?php
 
-function gameWithCells($n, $m) {
-    $rows = ceil($n / 2);
-    $columns = ceil($m / 2);
+function gameWithCells($n, $m) 
+{
+    if($n % 2 == 0)
+    {
+        $rows = $n/2;
+    }
+    else
+    {
+        $rows = ($n+1)/2;
+    }
+    if($m % 2 == 0)
+    {
+        $columns = $m/2;
+    }
+    else
+    {
+        $columns = ($m+1)/2;
+
+    }
     return $rows * $columns;
 
 
 }
+$n = int()readline("Enter the rows:");
+$m = int()readline("Enter the column:");
+$games = gameWithCells($n, $m);
+echo $games;
 
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
-
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-$n = intval($first_multiple_input[0]);
-
-$m = intval($first_multiple_input[1]);
-
-$result = gameWithCells($n, $m);
-
-fwrite($fptr, $result . "\n");
-
-fclose($fptr);
 ?>
